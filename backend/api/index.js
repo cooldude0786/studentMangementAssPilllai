5de2ffdc55d8714aa7ad52d2b54fe792706d8c02
Khizar shaikh <khizarshaikh2922@gmail.com>
2026-04-17 14:58:58 +0530

    updated config as per vercel

---
+++ backend/api/index.js
@@ -0,0 +1,4 @@
+// root/api/index.js
+import app from "../backend/src/app.js";
+
+export default app;
--- backend/src/server.js
+++ backend/src/server.js
@@ -3,7 +3,6 @@ dotenv.config();
 
 import app from "./app.js";
 const PORT = 5000;
-console.log(process.env.DATABASE_URL);
 app.get("/", (req, res) => {
   res.send("Welcome to the Student Management System API");
 });
